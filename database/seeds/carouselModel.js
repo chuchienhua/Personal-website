4f185a61f8790b35b566da06480d2a08a80e9548
Chu Chien-Hua <serre1234@gmail.com>
2021-04-28 21:21:19 +0800

    first

---
+++ database/seeds/carouselModel.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        image: '/sample/carousel1.jpg',
+        title: '2020 日本京都大學研討會',
+    },
+    {
+        image: '/sample/carousel2.jpg',
+        title: '腦波心動指數交友系統',
+    },
+    {
+        image: '/sample/carousel3.jpg',
+        title: '受邀參加大學專題展',
+    },
+];
